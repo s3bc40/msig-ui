bfb7057ec7a3781c83eb774af4e6130adf120a33
s3bc40 <s3bc40@proton.me>
2025-10-03 10:33:14 +0200

    fix: remove comments and legacy config

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
-// import {
-//   dehydrate,
-//   HydrationBoundary,
-//   QueryClient,
-// } from "@tanstack/react-query";
 import HomePageClient from "./HomePageClient";
-// import { getAccount } from "@wagmi/core";
-// import { getConfig } from "./config";
 
 export default async function HomePage() {
-  // const queryClient = new QueryClient();
-  // const config = getConfig();
-
-  // await queryClient.prefetchQuery({
-  //   queryKey: ["account", config],
-  //   queryFn: () => getAccount(config),
-  // });
   return <HomePageClient />;
 }
